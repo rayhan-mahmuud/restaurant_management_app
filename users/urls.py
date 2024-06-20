170e1828afdb2ba2245c4f228866bdbc12a379b1
Rayhan Mahmud <rayhan.mahmuud@gmail.com>
2024-06-20 12:42:41 +0600

    Add login logout and Save the users to DB

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from .forms import SignupForm
+from django.contrib import messages
+from django import views
+from django.contrib.auth import logout
 
 
 def signup(request):
-    form = SignupForm
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.add_message(request, level=200, message=f"Hi {username}! Your account is created successfully.")
+
+    else:
+        form = SignupForm()
     return render(request, 'signup.html', {'form': form})
+
+
+class LogoutView(views.View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'logout.html')
+
+
+
